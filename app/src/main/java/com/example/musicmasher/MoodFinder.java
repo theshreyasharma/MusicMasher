112070cc904bf7bb74848576564b82721a012c05
uvserpent <tasia.a.davis@gmail.com>
2021-11-18 19:33:37 -0600

    updated onClick intent for MoodFinder

--- app/src/main/java/com/example/musicmasher/MoodFinder.java
+++ app/src/main/java/com/example/musicmasher/MoodFinder.java
@@ -3,9 +3,11 @@ package com.example.musicmasher;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -35,8 +37,16 @@ public class MoodFinder extends AppCompatActivity {
 
             }
         });
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("MOOD", selection[0]);
-        startActivity(intent);
+
+        Button updateMoodButton = findViewById(R.id.button3);
+        updateMoodButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                intent.putExtra("MOOD", selection[0]);
+                startActivity(intent);
+            }
+        });
+
     }
 }
