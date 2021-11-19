6b6fe3fa3d9ce98d4d86c6035efdde7532a3a138
uvserpent <tasia.a.davis@gmail.com>
2021-11-18 20:03:37 -0600

    fixed mash button link to mash page

--- app/src/main/java/com/example/musicmasher/PFPActivity.java
+++ app/src/main/java/com/example/musicmasher/PFPActivity.java
@@ -1,8 +1,13 @@
 package com.example.musicmasher;
 
+import android.content.Intent;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PFPActivity extends AppCompatActivity {
@@ -18,6 +23,16 @@ public class PFPActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pfp);
+        //get mood string
+        Intent intent = getIntent();
+        String mood = "none";
+        if(getIntent().getExtras() != null) {
+            mood = intent.getStringExtra("mood");
+        }
+        //set current Mood text
+        TextView currentMood = findViewById(R.id.current_mood);
+        currentMood.setText("Current  Mood: " + mood);
+        //populate recap list w/ hardcoded data
         lv = findViewById(R.id.recap_list);
         ArrayAdapter<String> lv_arr;
         lv_arr = new ArrayAdapter<>(
@@ -25,5 +40,26 @@ public class PFPActivity extends AppCompatActivity {
                            R.layout.support_simple_spinner_dropdown_item,
                            recap_list);
         lv.setAdapter(lv_arr);
+        //link mash, mood, listening history buttons to activities
+        Button moodButton = findViewById(R.id.mood_button);
+        Button mashButton = findViewById(R.id.mash_button);
+        moodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MoodFinder.class);
+                startActivity(intent);
+            }
+        });
+
+        String finalMood = mood;
+        mashButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Mash.class);
+                intent.putExtra("MOOD", finalMood);
+                startActivity(intent);
+            }
+        });
+
     }
 }
