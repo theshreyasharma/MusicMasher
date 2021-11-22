5e950f24727235ffc3ed99381e5292cc1055ef10
Raj Panditls <rajpandit@Rajs-MBP.attlocal.net>
2021-11-22 13:44:31 -0800

    Added 6 more artists. Added space and cap error checking

--- app/src/main/java/com/example/musicmasher/Mash.java
+++ app/src/main/java/com/example/musicmasher/Mash.java
@@ -80,29 +80,41 @@ public class Mash extends AppCompatActivity {
         }
     public void mashMe(View view) {
         HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
-        map.put("Drake", new ArrayList<String>(
-                Arrays.asList("DrakeSong1", "DrakeSong2")));
-        map.put("Kanye", new ArrayList<String>(
-                Arrays.asList("KanyeSong1", "KanyeSong2")));
+
+        /**
+         * To whomever wants to help me add artists,
+         * add their key as ALL lowercase please.
+         */
+        map.put("drake", new ArrayList<String>(
+                Arrays.asList("God's Plan", "Way 2 Sexy", "Toosie Slide", "Know Yourself", "One Dance", "Laugh Now Cry Later")));
+        map.put("kanye", new ArrayList<String>(
+                Arrays.asList("Praise God", "Flashing Lights", "Off the Grid", "Heartless")));
+        map.put("taylor swift", new ArrayList<String>(
+                Arrays.asList("Blank Space", "Look What You Made Me Do", "You Belong With Me", "Bad Blood")));
+        map.put("ariana grande", new ArrayList<String>(
+                Arrays.asList("Thank you next", "God Is a Woman", "Rain on Me", "Bang Bang")));
+        map.put("justin beiber", new ArrayList<String>(
+                Arrays.asList("Baby", "Holy", "STAY", "LONELY", "Love Yourself ", "Mood")));
+        map.put("adele", new ArrayList<String>(
+                Arrays.asList("Easy on Me", "Rolling in the Deep", "Someone Like You", "Send My Love", "Set Fire to the Rain")));
+        map.put("mariah carey", new ArrayList<String>(
+                Arrays.asList("All I want for Christmas is raj pandit", "We Belong Together", "Without You", "Always be My Baby", "Obsessed")));
+        map.put("madonna", new ArrayList<String>(
+                Arrays.asList("Hung up", "Frozen ", "Hung up", "The Power of Goodbye")));
         EditText artist = (EditText) findViewById(R.id.editTextTextPersonName2);
         EditText artist1 = (EditText) findViewById(R.id.editTextTextPersonName);
         Log.d("artist", artist.getText().toString());
         ArrayList<String> songs1 = null;
         ArrayList<String> songs2 = null;
 
-        if(map.containsKey(artist.getText().toString())&& map.containsKey(artist1.getText().toString())) {
-            songs1 = map.get(artist.getText().toString());
-            songs2 = map.get(artist1.getText().toString());
-
-
+        if(map.containsKey(artist.getText().toString().toLowerCase().trim())&& map.containsKey(artist1.getText().toString().toLowerCase().trim())) {
+            songs1 = map.get(artist.getText().toString().toLowerCase().trim());
+            songs2 = map.get(artist1.getText().toString().toLowerCase().trim());
             ArrayList<String> result = new ArrayList<String>();
             int bound = Math.min(songs1.size(), songs2.size());
             for (int i = 0; i < bound; i++) {
                 result.add(songs1.get(i) + "x" + songs2.get(i));
-
             }
-
-
             String wow = result.toString();
             Bundle passMashed = new Bundle();
             Intent passToNewPage = new Intent(view.getContext(), PlaylistGenerated.class);
@@ -124,7 +136,6 @@ public class Mash extends AppCompatActivity {
                     .show();
         }
     }
-
     public void checkBoxFunction(View view) {
         boolean mood = true;
         CheckBox cb1 = (CheckBox) findViewById(R.id.checkBox);
