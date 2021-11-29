96344a4b53f5f0ab83d05d143c4f5a3a868aac59
saketv2 <saketv2@illinois.edu>
2021-11-29 01:22:56 -0600

    clean up login java file pt2

--- app/src/main/java/com/example/musicmasher/Login.java
+++ app/src/main/java/com/example/musicmasher/Login.java
@@ -17,9 +17,8 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
 
-    TextInputLayout phoneNumber, password;
+    TextInputLayout email, password;
     RelativeLayout progressbar;
-    CheckBox rememberMe;
     TextInputEditText phoneNumberEditText, passwordEditText;
 
     @Override
@@ -28,7 +27,7 @@ public class Login extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.welcome);
 
-        phoneNumber = findViewById(R.id.login_email);
+        email = findViewById(R.id.login_email);
         password = findViewById(R.id.login_password);
         progressbar = findViewById(R.id.login_progress_bar);
         phoneNumberEditText = findViewById(R.id.login_email_editText);
@@ -36,20 +35,9 @@ public class Login extends AppCompatActivity {
 
     }
 
-    /*
-    Login the
-    user in
-    app!
-     */
     public void logUserIn(View view) {
         startActivity(new Intent(getApplicationContext(), PFPActivity.class));
     }
-
-
-    public void callForgetPassword(View view) {
-        startActivity(new Intent(getApplicationContext(), PFPActivity.class));
-    }
-
 
     public void callSignUpFromLogin(View view) {
         startActivity(new Intent(getApplicationContext(), PFPActivity.class));
